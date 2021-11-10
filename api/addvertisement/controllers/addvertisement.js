30e5a67fd88703bde0a15ef4bec7d9de10b89480
someshshakya <somesh19@navgurukul.org>
2021-11-10 15:56:34 +0530

    added custom conttoller in addvertisement

--- api/addvertisement/controllers/addvertisement.js
+++ api/addvertisement/controllers/addvertisement.js
@@ -1,8 +1,36 @@
-'use strict';
-
+"use strict";
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-module.exports = {};
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.addvertisement.search(ctx.query);
+    } else {
+      entities = await strapi.services.addvertisement.find(ctx.query);
+    }
+
+    let data = entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.addvertisement })
+    );
+
+    data = data.map((img_obj) => {
+      let new_img = {};
+      new_img["img_url"] = img_obj["img_url"];
+      new_img["name"] = img_obj["name"];
+      new_img["id"] = img_obj["id"];
+      return new_img;
+    });
+    return data;
+  },
+};
--- api/addvertisement/services/addvertisement.js
+++ api/addvertisement/services/addvertisement.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-services)
  * to customize this service
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Promise to fetch all records
+   *
+   * @return {Promise}
+   */
+  find(params, populate) {
+    return strapi.query("addvertisement").find(params, populate);
+  },
+
+  /**
+   * Promise to search records
+   *
+   * @return {Promise}
+   */
+
+  search(params) {
+    return strapi.query("addvertisement").search(params);
+  },
+};
